c66fa3975c54c41be3be7d4aaed55f0b8f2effc1
Rudy Quevedo <iamsmarts@DESKTOP-0AGC3O4.localdomain>
2021-04-02 16:34:22 -0700

    updated footer links and added favicon

--- components/footer.js
+++ components/footer.js
@@ -3,14 +3,31 @@ import React from 'react'
 function Footer() {
   return (
     <div className="row footer justify-content-between">
-      <div className="col-6 col-md-4"><p>Angel City Brigade - 501 C 7</p></div>
+
+      <div className="col-6 col-md-4">
+        <p>Angel City Brigade - 501 C 7</p>
+      </div>
+
       <div className="col-6 col-md-4">
         <div className="row justify-content-between">
-          <div className="col sm-icons"><i aria-hidden className="fab fa-twitter"></i></div>
-          <div className="col sm-icons"><i aria-hidden className="fab fa-instagram"></i></div>
-          <div className="col sm-icons"><i aria-hidden className="fab fa-facebook"></i></div>
+          <div className="col sm-icons">
+            <a href="https://twitter.com/ACBrigade" target="_blank">
+              <i aria-hidden className="fab fa-twitter"></i>
+            </a>
+          </div>
+          <div className="col sm-icons">
+            <a href="https://www.instagram.com/angelcitybrigade/" target="_blank">
+              <i aria-hidden className="fab fa-instagram"></i>
+            </a>
+          </div>
+          <div className="col sm-icons">
+            <a href="https://www.facebook.com/angelcitybrigade" target="_blank">
+              <i aria-hidden className="fab fa-facebook"></i>
+            </a>
+          </div>
         </div>
       </div>
+
     </div>
   )
 }
