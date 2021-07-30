c33a4cba98d20a22d775f90a09490dbc9f7428db
Rudy Quevedo <rudy@iamsmarts.com>
2021-07-29 17:12:48 -0700

    updating head

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,6 @@ const Layout = ({children, title, description, router}) =>(
 
 <div className={`container`}>
   <Head>
-    <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 
     <title>
@@ -17,7 +16,7 @@ const Layout = ({children, title, description, router}) =>(
     </title>
 
     <base href="https://acb.la"/>
-    <meta name="description" content="ACB are LA Galaxy's rowdiest and loudest supporter group. Join us in section 121 and make your voice heard and show support for MLS' biggest club."/>
+    <meta name="description" content="ACB - LA Galaxy's largest supporter group, stand with us in 121 and 122 support 90 minutes+"/>
     <link rel="canonical" href="https://acb.la"/>
 
     <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
@@ -31,11 +30,13 @@ const Layout = ({children, title, description, router}) =>(
     <meta property="og:image:alt" content="ACB in action, scarves up!" />
 
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:description" content="ACB are LA Galaxy's rowdiest and loudest supporter group. Join us in section 121 | 122 and make your voice heard and show support for MLS' biggest club." />
-    <meta name="twitter:title" content="Angel City Brigade - Los Angeles Galaxy Supporters" />
     <meta name="twitter:site" content="@acbrigade" />
-    <meta name="twitter:image" content="https://b121gade.com/wp-content/uploads/2021/03/IMG_2416-scaled.jpg" />
     <meta name="twitter:creator" content="@acbrigade" />
+    <meta property="og:url" content="https://acb.la" />
+    <meta property="og:title" content="121 Angel City Brigade 122" />
+    <meta property="og:description" content="ACB - LA Galaxy's largest supporter group, stand with us in 121 and 122 support 90 minutes+ " />
+    <meta property="og:image" content="/acb-stands-scarves.jpg" />
+
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity= "sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous" />
     <script src="https://kit.fontawesome.com/90ad3ade8e.js" crossOrigin="anonymous"/>
