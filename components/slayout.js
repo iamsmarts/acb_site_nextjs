1a63fc3251ebcd156e62ebeeef29189cd5f637a1
Rudy Quevedo <rudy@iamsmarts.com>
2021-08-17 06:06:05 -0700

    protest statement styles

---
+++ components/slayout.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Header from './header'
+import Footer from './footer'
+
+import { Children } from 'react'
+
+
+const Slayout = ({children, title, description, router}) =>(
+
+<div className={`cover-container d-flex h-100 p-3 mx-auto flex-column`}>
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+
+    <title>
+      {title}
+    </title>
+
+
+    <meta name="description" content="ACB - LA Galaxy's largest supporter group, stand with us in 121 and 122 support 90 minutes+"/>
+    <link rel="canonical" href="https://acb.la"/>
+
+    <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
+
+    <meta property="og:url" content="https://acb.la" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="ANGEL CITY BRIGADE | ACB.LA"/>
+    <meta property="og:title" content="Angel City Brigade" />
+    <meta property="og:description" content="ACB - LA Galaxy's largest supporter group, stand with us in 121 and 122 support 90 minutes+ " />
+    <meta property="og:image" content="/acb-stands-scarves-update.jpg" />
+    <meta property="og:image:alt" content="ACB in action, scarves up!" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@acbrigade" />
+    <meta name="twitter:image" content="https://b121gade.com/wp-content/uploads/2021/03/IMG_2416-scaled.jpg" />
+    <meta name="twitter:creator" content="@acbrigade" />
+    <meta property="og:url" content="https://acb.la" />
+    <meta property="og:title" content="121 Angel City Brigade 122" />
+    <meta property="og:description" content="ACB - LA Galaxy's largest supporter group, stand with us in 121 and 122 support 90 minutes+ " />
+    <meta property="og:image" content="/acb-stands-scarves-update.jpg" />
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity= "sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous" />
+    <script src="https://kit.fontawesome.com/90ad3ade8e.js" crossOrigin="anonymous"/>
+
+    <link rel="shortcut icon" href="/favicon_updated.ico" />
+  </Head>
+  {children}
+</div>
+
+)
+export default Slayout;
+
--- pages/8-17.js
+++ pages/8-17.js
@@ -1,14 +1,22 @@
+import Slayout from '../components/slayout';
+import parse from 'html-react-parser';
+import Image from 'next/image'
 export default function Protest(){
   return(
-    <main role="main" className="inner cover">
-        <p><strong>To the entire LA Galaxy Supporters Community and Fans,</strong></p>
+    <Slayout title="Angle City Brigade | About ACB">
+    <main role="main" className="inner cover statement">
+        <Image src="/vblock.png" alt="Victoria Block Logo"width="150" height="117" layout="intrinsic" />
+        <p>August 17, 2021</p>
+        <p>To the entire LA Galaxy Supporters Community and Fans,</p>
          <p>Dignity Health Sportspark is our home. As one of the country’s first soccer specific stadiums our community has been at the forefront of soccer culture in the US. The post game celebrations in lot 13 is something that has come together organically and has attracted more casual fans to become supporters of the LA Galaxy. </p>
          <p>Sunday August 8th, the front office along with DHSP general management and AEG appointed security leadership directed Los Angeles County Sheriffs officers to use high frequency sirens, threats of violence, and other unwarranted tactics to dismantle organic support for the the club.</p>
          <p>We emphatically oppose any and all attempts to work against our collective love and passion for our team. Furthermore, we condemn the use of force and harassment of street vendors who we welcome. </p>
          <p>Any attempts to suppress this organic support for the LA Galaxy is strongly opposed by the Angel City Brigade. The LA Galaxy, our stadium, and our support is a staple in Major League Soccer. The way we support our club is part of the evolution of soccer culture across North America. Its time the front office values our collective contributions. </p>
-        <h1 className="cover-heading">Cover your page.</h1>
-        <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+         <p><strong> - Angel City Brigade, Galaxians 96, Galaxy Outlawz </strong></p>
       </main>
+
+      </Slayout>
   )
 }
+
 
